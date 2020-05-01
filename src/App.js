fc2608aab26f603c41d8ef806ef9e9b4e32db2bc
Derek Etman <dereketman@gmail.com>
2020-04-30 19:50:40 -0700

    + I made a sound

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as Tone from "tone";
 import './App.css';
 
 function App() {
+  const synth = new Tone.AMSynth().toMaster()
+  
+
+  // synth.triggerAttackRelease("C4","8n");
+  synth.triggerAttackRelease("D5","8n",1);
+  synth.triggerAttackRelease("E5","8n",2);
+  synth.triggerAttackRelease("F5","8n",3);
+  synth.triggerAttackRelease("G5","8n",4);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
     </div>
   );
 }
