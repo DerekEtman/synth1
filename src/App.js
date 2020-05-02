766a75f5f7eb26283fa3ca012608d3e54878bdce
Derek Etman <dereketman@gmail.com>
2020-05-01 23:29:48 -0700

    added messages

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 
 function App() {
   const synth = new Tone.AMSynth().toMaster()
+  console.log("Hello there")
+  
   
 
-  // synth.triggerAttackRelease("C4","8n");
+  synth.triggerAttackRelease("C4","8n");
   synth.triggerAttackRelease("D5","8n",1);
   synth.triggerAttackRelease("E5","8n",2);
   synth.triggerAttackRelease("F5","8n",3);
