1cee79c7b4aa47e4581d0bcd10564e66f7d756f0
Derek Etman <dereketman@gmail.com>
2020-05-04 10:09:00 -0700

    Simple styling

--- src/App.js
+++ src/App.js
@@ -8,17 +8,23 @@ function App() {
   
   
 
-  synth.triggerAttackRelease("C4","8n");
-  synth.triggerAttackRelease("D5","8n",1);
-  synth.triggerAttackRelease("E5","8n",2);
-  synth.triggerAttackRelease("F5","8n",3);
-  synth.triggerAttackRelease("G5","8n",4);
+  // synth.triggerAttackRelease("C4","8n");
+  // synth.triggerAttackRelease("D5","8n",1);
+  // synth.triggerAttackRelease("E5","8n",2);
+  // synth.triggerAttackRelease("F5","8n",3);
+  // synth.triggerAttackRelease("G5","8n",4);
 
 
 
   return (
     <div className="App">
-      
+      <div className="App-header">
+        <h2>MAKE NOISE YOU STUPID FU-</h2>
+
+      </div>
+      <div className="App-body">
+        test
+      </div>
 
     </div>
   );
